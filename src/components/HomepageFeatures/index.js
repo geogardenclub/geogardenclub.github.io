18f2d358c004132fdc6d0d9ec602cf0ad778803d
Philip Johnson <johnson@hawaii.edu>
2024-04-29 10:06:59 -0500

    Update landing page and theme color

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Unlock insights from your personal experience',
-    png: '/img/icons/garden.png',
+    png: '/img/landing/outcomes.png',
     description: (
       <>
         Home gardening over multiple seasons yields many useful insights. GGC provides new ways to gather and reflect on your home garden's history in order to improve your future gardening outcomes.
@@ -13,20 +13,20 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Unlock the collective wisdom of your community',
-    png: '/img/icons/people-circle.png',
+    title: 'Discover the collective wisdom of your community',
+    png: '/img/landing/chapters.png',
     description: (
       <>
-        GGC facilitates the creation and management of local "communities of practice" allowing members to more easily share garden outcome data and best practices with each other.
+        GGC facilitates the creation and management of local "communities of practice" allowing members to come together and share best practices with each other.
       </>
     ),
   },
   {
     title: 'Improve local food production and practices',
-    png: '/img/icons/vegetables-circle.png',
+    png: '/img/landing/home.png',
     description: (
       <>
-        Home gardens are an important and underutilized resource for increasing community resilience, health, and emotional well-being. GGC provides new ways for home gardens and gardeners to create a "virtual" local community garden.
+        Home gardens are an important and underutilized resource for increasing community resilience, health, and emotional well-being. GGC provides new ways for gardeners to work together to improve local food production and practices.
       </>
     ),
   },
@@ -36,7 +36,7 @@ function Feature({png, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <img height="150px" src={png} />
+        <img height="500px" src={png} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
