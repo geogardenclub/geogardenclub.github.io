9449156becc59f9eb9ad26e37a479ae0ec5fa887
Philip Johnson <johnson@hawaii.edu>
2025-03-18 07:51:26 -1000

    checkpoint

--- src/components/LandingPanelThree/index.js
+++ src/components/LandingPanelThree/index.js
@@ -28,29 +28,25 @@ export function LandingPanelThreeOld() {
 
 export default function LandingPanelThree() {
     return (
-        <div style={{backgroundColor: '#e3ddc2'}}>
             <div className="row row--no-gutters">
 
                 <div className="col">
-                    <div className="hero shadow--lw">
-                        <div className="container">
+                    <div className="hero">
+                        <div className="container text--center">
                             <h1 className="hero__title">New to GeoGardenClub?</h1>
 
                             <div>
-                                <Button style={{marginBottom: '2em'}} size='lg' variant="success" label="Get Started"
+                                <Button style={{marginBottom: '2em', marginTop: '1em'}} size='lg' variant="success" label="Get Started"
                                         link="/docs/user-guide/get-started/overview"/>
                             </div>
                         </div>
                     </div>
 
-                    <h1 style={{fontSize: '64px', color: '#02695c'}}>New2 to GeoGardenClub?</h1>
-
                 </div>
                 <div className="col" style={{marginBottom: "-20px"}}>
-                    <img src="img/landing/overshoulder.jpg"/>
+                    <img src="img/landing/overshoulder4.jpg"/>
                 </div>
 
             </div>
-        </div>
     );
 }
--- src/components/LandingPanelTwo/index.js
+++ src/components/LandingPanelTwo/index.js
@@ -4,29 +4,29 @@ export default function LandingPanelTwo() {
   return (
       <div className='hero hero--primary'>
       <div className="container">
-        <div className="row">
+        <div className="row" style={{paddingBottom: '32px'}}>
             <div className='col'>
                 <div className="text--center">
-                    <h1 style={{fontSize: '64px'}}>Join your local chapter today!</h1>
+                    <h1 className="hero__title">Join your local chapter today!</h1>
                 </div>
             </div>
         </div>
           <div className="row">
               <div className='col'>
                   <div className="text--center">
-                      <a href="https://apps.apple.com/us/app/geogardenclub/id6478464910?ign-itscg=30200&ign-itsct=apps_box_badge&mttnsubad=6478464910"><img height="200px" src="apple/black.svg"/></a>
+                      <a href="https://apps.apple.com/us/app/geogardenclub/id6478464910?ign-itscg=30200&ign-itsct=apps_box_badge&mttnsubad=6478464910"><img height="150px" src="apple/black.svg"/></a>
                   </div>
               </div>
               <div className='col'>
                   <div className="text--center">
-                      <a href="https://play.google.com/store/apps/details?id=com.geogardenclub.ggc_app"><img height="200px" src="google/google.svg"/></a>
+                      <a href="https://play.google.com/store/apps/details?id=com.geogardenclub.ggc_app"><img height="150px" src="google/google.svg"/></a>
                   </div>
               </div>
           </div>
-          <div className="row">
+          <div className="row" style={{paddingTop: '32px'}}>
               <div className='col'>
                   <div className="text--center">
-              <h1 style={{fontSize: '32px'}}>For a limited time, get the first six months free, then $4.99/month</h1>
+              <h1 className="hero__subtitle">For a limited time, get the first six months free, then $4.99/month</h1>
                   </div>
               </div>
           </div>
