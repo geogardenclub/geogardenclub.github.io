0f4736e695fc2857884f9244bd3d8cb29a35c9af
Philip Johnson <johnson@hawaii.edu>
2025-03-18 07:58:33 -1000

    checkpoint

--- src/components/LandingPanelOne/index.js
+++ src/components/LandingPanelOne/index.js
@@ -1,29 +1,24 @@
 import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
 import Button from '@site/src/components/Button';
-
 
 export default function LandingPanelOne() {
   return (
-      <div className={clsx('hero hero--primary', styles.heroBanner)} style={{backgroundImage: `url('/img/landing/garden-2b.jpeg')`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center'}}>
-    <section className={styles.features}>
+      <div className='hero hero--primary' style={{backgroundImage: `url('/img/landing/garden-2b.jpeg')`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center'}}>
       <div className="container">
         <div className="row">
-            <div className={clsx('col')}>
-                <div className={clsx('text--center padding-horiz--md')} >
+            <div className='col'>
+                <div className='text--center padding-horiz--md' >
                     <h1 style={{fontSize: '96px', color: 'white'}}>Grow and share more food.</h1>
                     <Button style={{marginBottom: '2em'}} size='lg' variant="success" label="Get Started" link="/docs/user-guide/get-started/overview" />
                 </div>
             </div>
-            <div className={clsx('col')}>
+            <div className='col'>
                 <div className="text--center">
                     <img height="400px" src="img/landing/seal1.png"/>
                 </div>
             </div>
         </div>
       </div>
-    </section>
       </div>
   );
 }
--- src/components/LandingPanelThree/index.js
+++ src/components/LandingPanelThree/index.js
@@ -1,30 +1,5 @@
 import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
 import Button from '@site/src/components/Button';
-import Columns from '@site/src/components/Columns';
-import Column from '@site/src/components/Column';
-
-export function LandingPanelThreeOld() {
-  return (
-      <div style={{backgroundColor: '#e3ddc2'}}>
-    <section className={styles.features}>
-
-        <div className={clsx('col')}>
-            <div className={clsx('text--center padding-horiz--md')} >
-                <h1 style={{fontSize: '64px', color: '#02695c'}}>New to GeoGardenClub?</h1>
-                <Button style={{marginBottom: '2em'}} size='lg' variant="success" label="Get Started" link="/docs/user-guide/get-started/overview" />
-            </div>
-        </div>
-
-        <div height="300px">
-            <img width="50%" src="img/landing/overshoulder.jpg"/>
-        </div>
-
-    </section>
-        </div>
-  );
-}
 
 export default function LandingPanelThree() {
     return (
